99a66b49b994ec7ee4679a5037cc79510424db9f
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2021-05-21 19:27:12 -0300

    update backend with categoria route

--- api/controllers/categoria.js
+++ api/controllers/categoria.js
@@ -3,15 +3,18 @@ const {create,list,del,edit} = require('../services/categoria.service');
 
 
 const createCategoria= async (req,res,next) => {
- 
+    const { body } = req;
+
+    await create(body)
 
     return res.status(200).send({
       mensagem: "Categoria registrado com sucesso."
     });
   }
   
-  const delCategoria = (req, res, next) => {
-  
+  const delCategoria = async (req, res, next) => {
+ const {id} = req.params
+ await del(id)
     return res.status(200).send({
       mensagem: "Categoria deletado com sucesso."
     });
@@ -19,12 +22,14 @@ const createCategoria= async (req,res,next) => {
   }
   const lista = async (req,res,next) => {
 
-  
-    return res.status(200).send(lista)
+  const response = await list()
+
+    return res.status(200).json(response)
   }
   const editCategoria = async (req,res,next) => {
-   
-  
+    const { body } = req;
+
+    await edit(body)
     return res.status(200).send({
       mensagem: "Categoria alterado com sucesso."
     });
--- api/services/categoria.service.js
+++ api/services/categoria.service.js
@@ -1,23 +1,23 @@
+const {openFile,createModel,Insert,Update,Delete} = require('../utils/database.utils')
+
 const create = async (body) => {
-  
-  
-  }
-  const edit = async (body) => {
- 
-  }
-  const del = async (body) => {
-  
-  
-  }
-  
-  const list = async (body) => {
-    
-  }
-  
-  module.exports = {
-    create,
-    list,
-    del,
-    edit
-  }
-  
+  await Insert('categoria',createModel(body))
+}
+const edit = async (body) => {
+await Update('categoria',body)
+}
+const del = async (body) => {
+
+await Delete('categoria',body)
+}
+
+const list = async (body) => {
+ return await openFile('categoria')
+}
+
+module.exports = {
+  create,
+  list,
+  del,
+  edit
+}
