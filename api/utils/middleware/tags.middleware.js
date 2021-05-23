1e576dfea7371d87873c7a5a027c68a7d7aaf199
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-05-22 21:32:05 -0300

    update code refactor

--- api/utils/middleware/tags.middleware.js
+++ api/utils/middleware/tags.middleware.js
@@ -7,34 +7,34 @@ function isArray(obj){
 
 
 const tagsHandler = async (req, res, next) =>{
-console.log(tagEnabled)
-const { body } = req;
-if(!tagEnabled){
+    console.log(tagEnabled)
+    const { body } = req;
+    if(!tagEnabled){
+        next()
+    }
+    let categoriaTag = []
+    console.log('middleware chamado',body)
+    var myCategory = await openFile('categoria')
+    console.log(myCategory)
+    if(isArray(body['categoria'])){
+        body['categoria'].map((item)=>{
+            let findedCategory  = myCategory.find((f)=>f.id == item)
+            tagsGenerator(findedCategory).map((c)=>{
+                categoriaTag.push(c)
+            })
+        })
+    }else{
+        
+        let findedCategory = myCategory.find((f)=>f.id == body['categoria'])
+        tagsGenerator(findedCategory).map((c)=>{
+            categoriaTag.push(c)
+        })
+        
+    }
+    req.body = { ...body,tags:[...tagsGenerator(body),...categoriaTag] }
+    
+    
     next()
-}
-let categoriaTag = []
-console.log('middleware chamado',body)
-var myCategory = await openFile('categoria')
-console.log(myCategory)
-if(isArray(body['categoria'])){
-    body['categoria'].map((item)=>{
-      let findedCategory  = myCategory.find((f)=>f.id == item)
-      tagsGenerator(findedCategory).map((c)=>{
-        categoriaTag.push(c)
-      })
-    })
-}else{
-  
-    let findedCategory = myCategory.find((f)=>f.id == body['categoria'])
-    tagsGenerator(findedCategory).map((c)=>{
-        categoriaTag.push(c)
-      })
-
-}
-req.body = { ...body,tags:[...tagsGenerator(body),...categoriaTag] }
-
-
-next()
 }
 
 module.exports = {
